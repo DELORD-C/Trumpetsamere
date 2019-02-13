b183535e48adcb3885826a62058356228978e4b8
DELORD Clément <clement.delord@epitech.eu>
2019-02-13 16:54:13 +0100

    merge resolve

--- Goku/js/states/TiledState.js
+++ Goku/js/states/TiledState.js
@@ -118,11 +118,7 @@ Platformer.TiledState.prototype.restart_level = function () {
 Platformer.TiledState.prototype.game_over = function () { 
     "use strict";
     localStorage.clear();
-<<<<<<< HEAD
     this.game.state.start("BootState", true, false, "Goku/assets/levels/level1.json");
-=======
-    this.game.state.start("BootState", true, false, "Luffy/assets/levels/level1.json");
->>>>>>> Map
 };
 
 Platformer.TiledState.prototype.init_hud = function () {
