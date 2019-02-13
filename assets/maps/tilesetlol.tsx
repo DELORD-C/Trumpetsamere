21e46b0890c5a3b13f78d37acda67032d620dddf
DELORD Clément <clement.delord@epitech.eu>
2019-02-13 11:47:15 +0100

    All fonctionnal (lvl4 pourri)

--- assets/maps/tilesetlol.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="tilesetlol" tilewidth="35" tileheight="35" spacing="1" tilecount="156" columns="12">
- <image source="../images/tiles_spritesheet.png" width="457" height="468"/>
-</tileset>
